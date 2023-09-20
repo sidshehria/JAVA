c5e56b612c03cfb21fcf738e410c803229b4a071
Siddharth Shehria <83392744+sidshehria@users.noreply.github.com>
2023-09-20 12:17:21 +0530

    Add files via upload

---
+++ javatype.java
@@ -0,0 +1,21 @@
+public class javatype {
+    public static void main(String[] args){
+        //Strings
+        //Concatenate
+        String name1 = "Siddharth";
+        String name2 = "Aditi";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+        System.out.println(name1.charAt(0));
+        System.out.println(name1.length());
+        //replace
+        String name4 = name1.replace("a","b");
+        System.out.println(name4);
+        //substring
+        System.out.println(name1.substring(0, 4));
+        
+
+        
+    }
+    
+}
